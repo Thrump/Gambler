06d486a44286ab26b789c8c35f9ff6f00f692543
Thrump <vonchude@gmail.com>
2020-06-23 08:57:40 -0400

    Created an error listener

---
+++ src/listeners/errorListener.js
@@ -0,0 +1,16 @@
+const { Listener } = require('discord-akairo');
+
+class ErrorListener extends Listener {
+    constructor() {
+        super('error', {
+            emitter: 'commandHandler',
+            event: 'error'
+        });
+    }
+
+    exec(error, message, command) {
+        message.channel.send(`ERROR: something broke in the backend.\n Report ${command.aliases} command to ☆彡#1001`);
+    }
+}
+
+module.exports = ErrorListener;
