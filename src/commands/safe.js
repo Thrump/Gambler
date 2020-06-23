0a0c84842d0eeabe6cb68ab121a7c94a41455a69
Thrump <vonchude@gmail.com>
2020-06-23 08:57:32 -0400

    Now lets you choose your site

--- src/commands/safe.js
+++ src/commands/safe.js
@@ -2,27 +2,38 @@ const { Command } = require("discord-akairo");
 const Booru = require("booru");
 const { MessageAttachment } = require("discord.js");
 
-class PictureCommand extends Command {
+class SafeCommand extends Command {
     constructor() {
         super('safe', {
-            aliases: ['safe'],
+            aliases: ['safe', 's'],
             cooldown: 3000,
             ratelimit: 2,
             args: [{
                 id: 'tag',
+                match: 'rest',
                 default: ''
-
+            }, {
+                id: 'site',
+                match: 'option',
+                flag: 'site:',
+                type: [
+                    ['e9', 'e926'],
+                    ['kn', 'konan', 'knet'],
+                    ['sb', 'safe', 'safebooru'],
+                    ['tb', 'tbib', 'big']
+                ],
+                default: 'tbib'
             }]
         })
     }
 
     async exec(message, args) {
-        const posts = await Booru.search('safebooru', [args.tag], { limit: 1, random: true });
-        console.log(posts);
+        var posts = await Booru.search(args.site, args.tag.split(' '), { limit: 1, random: true });
         if (posts.length == 0) return message.channel.send('ERROR: No pictures exist with that tag.');
+        while (posts.posts[0].rating != 's') posts = await Booru.search(args.site, args.tag.split(' '), { limit: 1, random: true });
         const attachment = new MessageAttachment(posts.first.fileUrl);
         message.channel.send(attachment);
     }
 }
 
-module.exports = PictureCommand;
+module.exports = SafeCommand;
