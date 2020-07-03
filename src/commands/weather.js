deec0644a83e1db77eeb372747d79c61de2fc27f
Thrump <vonchude@gmail.com>
2020-07-03 18:51:27 -0400

    Change Weather catergory to Utility, and removed ownerOnly

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -6,8 +6,7 @@ class WeatherCommand extends Command {
     constructor() {
         super('weather', {
             aliases: ['weather'],
-            ownerOnly: true,
-            category: 'development',
+            category: 'Utility',
             description: {
                 desc: "Displays the weather at a location",
                 format: "$weather (zip | city name) ",
@@ -22,29 +21,27 @@ class WeatherCommand extends Command {
 
     async exec(message, args) {
         var json;
-        if(args.arg1 > 0 && args.arg1 < 100000){
+        if (args.arg1 > 0 && args.arg1 < 100000) {
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${args.arg1}&appid=${process.env.WET}`);
             json = await response.json();
 
-        } 
-        else if(/\d/.test(args.arg1)){
+        } else if (/\d/.test(args.arg1)) {
             //string has number   
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${args.arg1}&appid=${process.env.WET}`);
             //put it in the zip and make it fail
             json = await response.json();
-        } 
-        else {
+        } else {
             //is some string with no numbers    
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${args.arg1}&appid=${process.env.WET}`);
             json = await response.json();
         }
         //code 404 or 400
-        if(json.cod == 404 || json.cod == 400){
+        if (json.cod == 404 || json.cod == 400) {
             message.channel.send(`Invalid entry, please enter Zip Code or City Name`);
         } else {
-            var temp = ((json.main.temp - 273.15)*1.8+32).toFixed(0);
-            var high = ((json.main.temp_max - 273.15)*1.8+32).toFixed(0);
-            var low = ((json.main.temp_min - 273.15)*1.8+32).toFixed(0);
+            var temp = ((json.main.temp - 273.15) * 1.8 + 32).toFixed(0);
+            var high = ((json.main.temp_max - 273.15) * 1.8 + 32).toFixed(0);
+            var low = ((json.main.temp_min - 273.15) * 1.8 + 32).toFixed(0);
             const embed = {
                 title: `Weather in ${json.name}`,
                 description: `Condtion: ${json.weather[0].description}`,
@@ -59,7 +56,7 @@ class WeatherCommand extends Command {
                     value: `${low}F`
                 }],
             }
-            message.channel.send({embed});
+            message.channel.send({ embed });
         }
     }
 }
