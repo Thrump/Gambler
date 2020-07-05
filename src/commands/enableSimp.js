507e42ff8245279e6f9970ddc81a06940e3b8f05
isahiz <isabelle.hizon@gmail.com>
2020-07-04 23:25:44 -0400

    added extra flag options for enable/disable, implemented status option to check if simpListener is enabled/disabled

--- src/commands/enableSimp.js
+++ src/commands/enableSimp.js
@@ -8,23 +8,23 @@ class EnableSimpListener extends Command {
             aliases: ['simp'],
             description: {
                 desc: "Enable or disable this server's Simp Listener",
-                format: "$enable-simp (true/false)",
-                example: "$enable-simp true"
+                format: "$simp (flag)",
+                example: "$simp --enable"
             },
             args: [{
                     id: 'enable',
                     match: 'flag',
-                    flag: '--enable'
+                    flag: ['--enable', '--e']
                 },
                 {
                     id: 'disable',
                     match: 'flag',
-                    flag: '--disable'
+                    flag: ['--disable', '--d']
                 },
                 {
                     id: 'status',
                     match: 'flag',
-                    flag: '--status'
+                    flag: ['--status', '--stat']
                 }
             ],
             category: 'Utility',
@@ -35,15 +35,17 @@ class EnableSimpListener extends Command {
     async exec(message, args) {
         let guild = await new Guild(message.guild.id).update();
         if (args.enable) {
-            //enable listener
             guild.setSimpListener(1);
-            return message.channel.send('Simp listener is enabled');
+            return message.channel.send('Simp listener has been enabled.');
         } else if (args.disable) {
-            //disable listener
             guild.setSimpListener(0);
-            return message.channel.send('Simp listener is disabled');
+            return message.channel.send('Simp listener has been disabled.');
         } else if (args.status){
-            //probably make a function to get the simp_listener value from the database
+            if (guild.simpListener == 0) {
+                return message.channel.send('Simp listener is disabled.');
+            } else if (guild.simpListern == 1) {
+                return message.channel.send('Simp listener is enabled.');
+            }
         } else {
             return message.channel.send('Please provide a flag.');
         }
