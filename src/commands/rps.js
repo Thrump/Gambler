3a5957cf7abb148537c0b650e7b4d0aef693f5b1
isahiz <isabelle.hizon@gmail.com>
2020-07-26 16:35:50 -0400

    bot move is decided using rng, game is playable

--- src/commands/rps.js
+++ src/commands/rps.js
@@ -4,13 +4,15 @@ const { MessageEmbed } = require('discord.js');
 User = require('../models/user').User
 client = require('../../main').client
 
+const botOptions = ['rock', 'paper', 'scissors'];
+
 class RPSCommand extends Command {
     constructor() {
         super('rps', {
             aliases: ['rps'],
             ownerOnly: false,
             description: {
-                desc: "Gamble your money by playing a game of Rock Paper Scissors.",
+                desc: "Gamble your money by playing a game of Rock-Paper-Scissors.",
                 format: "$rps (R/P/S) (amount)",
                 example: "$rps P 50"
             },
@@ -41,6 +43,45 @@ class RPSCommand extends Command {
 
         if (args.amount == 0)
             return message.channel.send('ERROR: Cannot bet 0');
+
+        let userWin = false;
+        let tie = false;
+        let netGain = 0;
+        let rand = Math.floor((Math.random() * 3) + 1);
+        let botOption = botOptions[rand - 1];
+
+        if (botOption == args.option) 
+            tie = true;
+        else if (botOption == 'rock' && args.option == 'paper' || botOption == 'paper' && args.option == 'scissors' || botOption == 'scissors' && args.option == 'rock')
+            userWin = true;
+
+        if (userWin && !tie) {
+            netGain = args.amount;
+            user.setWins(user.wins + 1);
+        } else if (!userWin && !tie) {
+            netGain = args.amount * -1;
+            user.setLosses(user.losses + 1);
+        }
+
+        user.setCurrency(user.currency + netGain);
+
+        const embed = new MessageEmbed();
+
+        embed.setTitle('Rock-Paper-Scissors result');
+        embed.setColor(`#C4FAF8`);
+
+        if (netGain != 0) {
+            let net = netGain > 0 ? 'gained' : 'lost';
+            embed.setDescription(`You ${net} ${Math.abs(netGain)} coins.`);
+        } else {
+            embed.setDescription('No change in your coin amount.');
+        }
+
+        embed.addField('Your move: ', args.option, false);
+        embed.addField('Opponent\'s move: ', botOption, false);
+
+        message.channel.send({ embed });
+
     }
 }
 
