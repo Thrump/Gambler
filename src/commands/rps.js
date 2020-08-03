321c37e3d20ed840c8b072ca8431fc4a03c7f611
isahiz <43762178+isahiz@users.noreply.github.com>
2020-08-03 08:12:57 -0400

    Dev rps (#52)
    * created rps command class, added description and args
    
    * bot move is decided using rng, game is playable
    
    * embed description states whether it was a win/loss/tie
    
    * fix typo
    
    * added images
    
    * error messages are in an embed
    
    * updated calculation for payout
    
    * actually resolved the conflicts
    
    * changed error msg embed from title text to description text
    
    * Revert "changed error msg embed from title text to description text"
    
    This reverts commit 4db3ff4df8190bbda24e75041e4b0d44797e496d.
    
    * changed message embed to description instead of title for errors
    
    * user can bet half or all on rps

--- src/commands/rps.js
+++ src/commands/rps.js
@@ -30,7 +30,6 @@ class RPSCommand extends Command {
                 ],
             }, {
                 id: 'amount',
-                type: 'number',
                 default: 0
             }],
             category: 'Money'
@@ -43,41 +42,35 @@ class RPSCommand extends Command {
         const embed = new MessageEmbed();
         embed.setColor(`#C4FAF8`);
 
+        if (args.amount < 1 || (isNaN(args.amount) && (args.amount != 'all' && args.amount != 'half'))) {
+            embed.setDescription('ERROR: Amount must be higher than 0');
+            return message.channel.send(embed);
+        }
+
         if (args.option != 'rock' && args.option != 'paper' && args.option != 'scissors') {
-            embed.setTitle('ERROR: Invalid argument provided for rock/paper/scissors option');
+            embed.setDescription('ERROR: Invalid argument provided for rock/paper/scissors option');
             return message.channel.send({ embed });
         }
 
         if (user.currency < args.amount || (args.amount == 'all' && user.currency == 0)) {
-            embed.setTitle('ERROR: Insufficient funds');
+            embed.setDescription('ERROR: Insufficient funds');
             return message.channel.send({ embed });
         }
 
-        if (args.amount == 0) {
-            embed.setTitle('ERROR: Cannot bet 0')
-            return message.channel.send({ embed });
+        let userBetAmt = 0;
+
+        if (args.amount == 'all') {
+            userBetAmt = user.currency;
+        } else if (args.amount == 'half') {
+            userBetAmt = Math.floor(user.currency / 2);
+        } else {
+            userBetAmt = parseInt(args.amount);
         }
 
-        let userWin = false;
-        let tie = false;
-        let netGain = 0;
+        user.setCurrency(user.currency - userBetAmt); // User's bet is subtracted from their current amount
+
         let rand = Math.floor((Math.random() * 3) + 1);
         let botOption = botOptions[rand - 1];
-
-        if (botOption == args.option) 
-            tie = true;
-        else if ((botOption == 'rock' && args.option == 'paper') || (botOption == 'paper' && args.option == 'scissors') || (botOption == 'scissors' && args.option == 'rock'))
-            userWin = true;
-
-        if (userWin && !tie) {
-            netGain = args.amount;
-            user.setWins(user.wins + 1);
-        } else if (!userWin && !tie) {
-            netGain = args.amount * -1;
-            user.setLosses(user.losses + 1);
-        }
-
-        user.setCurrency(user.currency + netGain);
 
         const canvas = createCanvas(200, 100);
         const ctx = canvas.getContext('2d');
@@ -88,21 +81,25 @@ class RPSCommand extends Command {
         ctx.drawImage(botImg, 0, 0, 100, canvas.height);
         ctx.drawImage(userImg, 100, 0, 100, canvas.height);
 
+        embed.setTitle('Rock-Paper-Scissors Result');
         const attachment = new MessageAttachment(canvas.toBuffer(), 'rps.png');
         embed.attachFiles(attachment).setImage('attachment://rps.png');
 
-        embed.setTitle('Rock-Paper-Scissors Result');
-
-        if (netGain > 0) {
-            embed.setDescription(`**You won!** \nYou gained ${Math.abs(netGain)} ${coinEmoji}`);
-        } else if (netGain < 0) {
-            embed.setDescription(`**You lost :(** \nYou lost ${Math.abs(netGain)} ${coinEmoji}`);
-        } else {
-            embed.setDescription(`**You tied!** \nNo change to your ${coinEmoji}`);
-        }
-
         embed.addField('Opponent: ', botOption, true);        
         embed.addField('You: ', args.option, true);
+
+        if (botOption == args.option) {
+            user.setCurrency(user.currency + userBetAmt); // User ties, gains back what they bet
+            embed.setDescription(`**You tied!** \nNo change to your ${coinEmoji}`);
+        } else if ((botOption == 'rock' && args.option == 'paper') || (botOption == 'paper' && args.option == 'scissors') || (botOption == 'scissors' && args.option == 'rock')) {
+            user.setCurrency(user.currency + userBetAmt * 2); // User wins, gains double what they bet
+            user.setWins(user.wins + 1);
+            embed.setDescription(`**You won!** \nYou gained ${userBetAmt} ${coinEmoji}`);
+        } else {
+            // User loses, gains no money back
+            user.setLosses(user.losses + 1);
+            embed.setDescription(`**You lost :(** \nYou lost ${userBetAmt} ${coinEmoji}`);
+        }
 
         message.channel.send({ embed });
 
