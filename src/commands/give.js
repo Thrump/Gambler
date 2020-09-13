e49df26ea246b941735cc1210186f3cae5330b90
Thrump <vonchude@gmail.com>
2020-09-13 01:15:03 -0400

    Revert "Changed coin image"
    This reverts commit c0c37b60ead7e4dd7c229e62544d674d2f0de827.

--- src/commands/give.js
+++ src/commands/give.js
@@ -1,109 +1,52 @@
 const { Command } = require('discord-akairo');
-const { MessageEmbed } = require('discord.js');
-const { MessageAttachment } = require('discord.js');
-const { createCanvas, loadImage } = require('canvas');
+const { User } = require('../models/user');
 
-User = require('../models/user').User
-client = require('../../main').client
-
-const coinEmoji = "<:coin:754568123201618011>";
-
-const botOptions = ['rock', 'paper', 'scissors'];
-const imgLinks = ["https://cdn.discordapp.com/emojis/737275263129354251.png?v=1", "https://cdn.discordapp.com/emojis/737288503490117713.png?v=1", "https://cdn.discordapp.com/emojis/737289554137972806.png?v=1"];
-
-class RPSCommand extends Command {
+const coinEmoji = "<:coins:729903134536630314>";
+class GiveCommand extends Command {
     constructor() {
-        super('rps', {
-            aliases: ['rps'],
-            ownerOnly: false,
+        super('give', {
+            aliases: ['give', 'gv'],
+            ownerOnly: true,
+            channel: 'guild',
             description: {
-                desc: "Gamble your money by playing a game of Rock-Paper-Scissors.",
-                format: "$rps (R/P/S) (amount)",
-                example: "$rps P 50"
+                desc: "Gives money to users",
+                format: "$give (amount) ({@users})",
+                example: "$give 1000 @☆彡",
             },
             args: [{
-                id: 'option',
-                type: [
-                    ['rock', 'r'],
-                    ['paper', 'p'],
-                    ['scissors', 'scissor', 's']
-                ],
-            }, {
                 id: 'amount',
+                type: 'number',
                 default: 0
+            }, {
+                id: 'users',
+                type: 'member',
+                match: 'separate'
             }],
             category: 'Money'
-        });
+        })
     }
 
-    async exec(message, args) {
-        let user = await new User(message.author.id).update();
 
-        const embed = new MessageEmbed();
-        embed.setColor(`#C4FAF8`);
 
-        if (args.amount < 1 || (isNaN(args.amount) && (args.amount != 'all' && args.amount != 'half'))) {
-            embed.setDescription('ERROR: Amount must be higher than 0');
-            return message.channel.send(embed);
+    exec(message, args) {
+
+        const embed = {
+            color: `#C4FAF8`
         }
-
-        if (args.option != 'rock' && args.option != 'paper' && args.option != 'scissors') {
-            embed.setDescription('ERROR: Invalid argument provided for rock/paper/scissors option');
-            return message.channel.send({ embed });
-        }
-
-        if (user.currency < args.amount || (args.amount == 'all' && user.currency == 0)) {
-            embed.setDescription('ERROR: Insufficient funds');
-            return message.channel.send({ embed });
-        }
-
-        let userBetAmt = 0;
-
-        if (args.amount == 'all') {
-            userBetAmt = user.currency;
-        } else if (args.amount == 'half') {
-            userBetAmt = Math.floor(user.currency / 2);
+        if (args.users == null) {
+            embed.title = `ERROR`;
+            embed.description = `Need to @ users`;
+        } else if (args.users.length != message.mentions.members.array().length) {
+            embed.title = `ERROR`;
+            embed.description = `Need to give amount to award`;
         } else {
-            userBetAmt = parseInt(args.amount);
+            args.users.forEach(async element => {
+                let user = await new User(element.user.id).update();
+                user.setCurrency(user.currency + args.amount);
+            });
+            embed.description = `**User(s) have been awarded with ${args.amount} ${coinEmoji}**`;
         }
-
-        user.setCurrency(user.currency - userBetAmt); // User's bet is subtracted from their current amount
-
-        let rand = Math.floor((Math.random() * 3) + 1);
-        let botOption = botOptions[rand - 1];
-
-        const canvas = createCanvas(200, 100);
-        const ctx = canvas.getContext('2d');
-
-        const botImg = await loadImage(imgLinks[rand - 1]);
-        const userImg = await loadImage(imgLinks[botOptions.indexOf(args.option)]);
-
-        ctx.drawImage(botImg, 0, 0, 100, canvas.height);
-        ctx.drawImage(userImg, 100, 0, 100, canvas.height);
-
-        embed.setTitle('Rock-Paper-Scissors Result');
-        const attachment = new MessageAttachment(canvas.toBuffer(), 'rps.png');
-        embed.attachFiles(attachment).setImage('attachment://rps.png');
-
-        embed.addField('Opponent: ', botOption, true);
-        embed.addField('You: ', args.option, true);
-
-        if (botOption == args.option) {
-            user.setCurrency(user.currency + userBetAmt); // User ties, gains back what they bet
-            embed.setDescription(`**You tied!** \nNo change to your ${coinEmoji}`);
-        } else if ((botOption == 'rock' && args.option == 'paper') || (botOption == 'paper' && args.option == 'scissors') || (botOption == 'scissors' && args.option == 'rock')) {
-            user.setCurrency(user.currency + userBetAmt * 2); // User wins, gains double what they bet
-            user.setWins(user.wins + 1);
-            embed.setDescription(`**You won!** \nYou gained ${userBetAmt} ${coinEmoji}`);
-        } else {
-            // User loses, gains no money back
-            user.setLosses(user.losses + 1);
-            embed.setDescription(`**You lost :(** \nYou lost ${userBetAmt} ${coinEmoji}`);
-        }
-
         message.channel.send({ embed });
-
     }
 }
-
-module.exports = RPSCommand;
+module.exports = GiveCommand;
--- src/commands/roll.js
+++ src/commands/roll.js
@@ -6,30 +6,30 @@ const { createCanvas, loadImage } = require('canvas');
 User = require('../models/user').User
 client = require('../../main').client
 
-const coinEmoji = "<:coin:754568123201618011>";
+const coinEmoji = "<:coins:729903134536630314>";
 
-const botOptions = ['rock', 'paper', 'scissors'];
-const imgLinks = ["https://cdn.discordapp.com/emojis/737275263129354251.png?v=1", "https://cdn.discordapp.com/emojis/737288503490117713.png?v=1", "https://cdn.discordapp.com/emojis/737289554137972806.png?v=1"];
+const diceImgLinks = ["https://cdn.discordapp.com/emojis/730765874763399168.png?v=1", "https://cdn.discordapp.com/emojis/730765922062827535.png?v=1", "https://cdn.discordapp.com/emojis/730765974999138377.png?v=1", "https://cdn.discordapp.com/emojis/730765993210544128.png?v=1", "https://cdn.discordapp.com/emojis/730766014349836369.png?v=1", "https://cdn.discordapp.com/emojis/730766033723457587.png?v=1"];
 
-class RPSCommand extends Command {
+class RollCommand extends Command {
     constructor() {
-        super('rps', {
-            aliases: ['rps'],
+        super('roll', {
+            aliases: ['roll'],
             ownerOnly: false,
             description: {
-                desc: "Gamble your money by playing a game of Rock-Paper-Scissors.",
-                format: "$rps (R/P/S) (amount)",
-                example: "$rps P 50"
+                desc: "Gamble your money by guessing whether the sum of two dice rolled will be lower than seven, higher than seven, or is seven.",
+                format: "$roll (H/L/S) (amount)",
+                example: "$roll H 50"
             },
             args: [{
                 id: 'option',
                 type: [
-                    ['rock', 'r'],
-                    ['paper', 'p'],
-                    ['scissors', 'scissor', 's']
+                    ['high', 'h'],
+                    ['low', 'l'],
+                    ['seven', 'sevens', 's']
                 ],
             }, {
                 id: 'amount',
+                type: 'number',
                 default: 0
             }],
             category: 'Money'
@@ -42,69 +42,69 @@ class RPSCommand extends Command {
         const embed = new MessageEmbed();
         embed.setColor(`#C4FAF8`);
 
-        if (args.amount < 1 || (isNaN(args.amount) && (args.amount != 'all' && args.amount != 'half'))) {
-            embed.setDescription('ERROR: Amount must be higher than 0');
-            return message.channel.send(embed);
-        }
-
-        if (args.option != 'rock' && args.option != 'paper' && args.option != 'scissors') {
-            embed.setDescription('ERROR: Invalid argument provided for rock/paper/scissors option');
-            return message.channel.send({ embed });
+        if (args.option == null) {
+            embed.setTitle('ERROR: Invalid argument provided for high/low/seven option');
+            return message.channel.send({ embed })
         }
 
         if (user.currency < args.amount || (args.amount == 'all' && user.currency == 0)) {
-            embed.setDescription('ERROR: Insufficient funds');
+            embed.setTitle('ERROR: Insufficient funds');
             return message.channel.send({ embed });
         }
 
-        let userBetAmt = 0;
-
-        if (args.amount == 'all') {
-            userBetAmt = user.currency;
-        } else if (args.amount == 'half') {
-            userBetAmt = Math.floor(user.currency / 2);
-        } else {
-            userBetAmt = parseInt(args.amount);
+        if (args.amount == 0) {
+            embed.setTitle('ERROR: Cannot bet 0');
+            return message.channel.send({ embed });
         }
 
-        user.setCurrency(user.currency - userBetAmt); // User's bet is subtracted from their current amount
+        user.setCurrency(user.currency - args.amount); // User's bet is subtracted from current amount
 
-        let rand = Math.floor((Math.random() * 3) + 1);
-        let botOption = botOptions[rand - 1];
+        embed.setTitle('Roll Result');
+
+        let d1 = Math.floor((Math.random() * 6) + 1);
+        let d2 = Math.floor((Math.random() * 6) + 1);
 
         const canvas = createCanvas(200, 100);
         const ctx = canvas.getContext('2d');
 
-        const botImg = await loadImage(imgLinks[rand - 1]);
-        const userImg = await loadImage(imgLinks[botOptions.indexOf(args.option)]);
+        const d1Img = await loadImage(diceImgLinks[d1 - 1]);
+        const d2Img = await loadImage(diceImgLinks[d2 - 1]);
+        ctx.drawImage(d1Img, 0, 0, 100, canvas.height);
+        ctx.drawImage(d2Img, 100, 0, 100, canvas.height);
 
-        ctx.drawImage(botImg, 0, 0, 100, canvas.height);
-        ctx.drawImage(userImg, 100, 0, 100, canvas.height);
+        const attachment = new MessageAttachment(canvas.toBuffer(), 'diceRoll.png');
 
-        embed.setTitle('Rock-Paper-Scissors Result');
-        const attachment = new MessageAttachment(canvas.toBuffer(), 'rps.png');
-        embed.attachFiles(attachment).setImage('attachment://rps.png');
+        embed.attachFiles(attachment).setImage('attachment://diceRoll.png');
 
-        embed.addField('Opponent: ', botOption, true);
-        embed.addField('You: ', args.option, true);
-
-        if (botOption == args.option) {
-            user.setCurrency(user.currency + userBetAmt); // User ties, gains back what they bet
-            embed.setDescription(`**You tied!** \nNo change to your ${coinEmoji}`);
-        } else if ((botOption == 'rock' && args.option == 'paper') || (botOption == 'paper' && args.option == 'scissors') || (botOption == 'scissors' && args.option == 'rock')) {
-            user.setCurrency(user.currency + userBetAmt * 2); // User wins, gains double what they bet
-            user.setWins(user.wins + 1);
-            embed.setDescription(`**You won!** \nYou gained ${userBetAmt} ${coinEmoji}`);
-        } else {
-            // User loses, gains no money back
-            user.setLosses(user.losses + 1);
-            embed.setDescription(`**You lost :(** \nYou lost ${userBetAmt} ${coinEmoji}`);
+        let actualResult = '';
+        if (d1 + d2 > 7) {
+            actualResult = 'high';
+        } else if (d1 + d2 < 7) {
+            actualResult = 'low';
+        } else if (d1 + d2 == 7) {
+            actualResult = 'seven';
         }
 
-        message.channel.send({ embed });
+        let profit = 0;
+        if (args.option === actualResult && actualResult != 'seven') {
+            user.setCurrency(user.currency + args.amount * 2); // User wins (but prediction is not 7); gains twice what they bet
+            user.setWins(user.wins + 1);
+            embed.setDescription(`You gained ${parseInt(args.amount)} ${coinEmoji}`);
+        } else if (args.option === actualResult && actualResult == 'seven') {
+            user.setCurrency(user.currency + args.amount * 5); // User wins (prediction is 7); net gains 4x their bet
+            user.setWins(user.wins + 1);
+            embed.setDescription(`You gained ${parseInt(args.amount * 4)} ${coinEmoji}`);
+        } else {
+            // User gains nothing back.
+            user.setLosses(user.losses + 1);
+            embed.setDescription(`You lost ${parseInt(args.amount)} ${coinEmoji}`);
+        }
 
+        embed.addField('Your prediction: ', args.option, true);
+        embed.addField('Actual result: ', `${parseInt(d1 + d2)} (${actualResult})`, true);
+
+        message.channel.send({ embed });
     }
 }
 
-module.exports = RPSCommand;
 module.exports = RollCommand;
